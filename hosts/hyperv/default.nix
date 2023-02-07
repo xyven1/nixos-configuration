e9dfb3a49636c352f92360585a5d30244aa505f2
Xyven1 <maritimus987@gmail.com>
2023-02-07 21:12:27 +0000

    added boot.grub and fs config

--- hosts/hyperv/default.nix
+++ hosts/hyperv/default.nix
@@ -10,6 +10,20 @@
 	  kernelPackages = pkgs.linuxKernel.packages.linux_zen;
 	  binfmt.emulatedSystems = [ "aarch64-linux" ];
   };
+boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    zfsSupport = true;
+  };
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+  boot.zfs.devNodes = "/dev/disk/by-partuuid";
+  services.zfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+  };
 
   networking = {
     hostName = "hyperv";
