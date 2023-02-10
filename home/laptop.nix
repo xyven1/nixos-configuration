6b05dd0b36feebeef33993e73df1d21fdbfdb6ef
Xyven1 <maritimus987@gmail.com>
2023-02-10 11:47:13 -0500

    configuration for laptop

---
+++ home/laptop.nix
@@ -0,0 +1,25 @@
+{ inputs, outputs, lib, config, pkgs, ... }:
+
+{
+	imports = [
+		./default.nix
+	];
+
+	nixpkgs = {
+		config = {
+			allowUnfree = true;
+		};
+	};
+
+	home = {
+		username = "xyven";
+		homeDirectory = "/home/xyven";
+	};
+
+
+	programs.home-manager.enable = true;
+	programs.git.enable = true;
+	home.stateVersion = "22.11";
+}
+
+
