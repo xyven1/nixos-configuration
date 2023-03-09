df1739b554f9b7c59bd4bec5026830a9e804ea9e
xyven1 <maritimus987@gmail.com>
2023-03-09 02:35:37 +0000

    for ockham install

--- hosts/ockham/default.nix
+++ hosts/ockham/default.nix
@@ -2,16 +2,23 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./disko.nix
     ../common/global
     ../common/users/xyven
   ];
+  disko.devices = pkgs.callPackage ./disko.nix {
+    disks = [ "/dev/sda" ];
+  };
   
   users.users.xyven = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OUNpoctlB+kygCCqcP/YRPDzGcykblU5TKUnfKhY+ (none)"
     ];
   };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.openssh.enable = true;
 
   networking = {
     hostName = "ockham";
