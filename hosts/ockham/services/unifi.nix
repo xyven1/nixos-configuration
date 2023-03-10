33c3410c5102a8ef7e5d3cc821972195d4a301bf
xyven1 <maritimus987@gmail.com>
2023-03-10 01:21:16 -0500

    got unifi working

--- hosts/ockham/services/unifi.nix
+++ hosts/ockham/services/unifi.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 {
+  # this was needed, even with openFirewall = true, to get the controller to get through the backup restore and intial login
+  # networking.firewall.allowedTCPPorts = [ 8080 8443 ];
   services.unifi = {
     enable = true;
-    unifiPackage = pkgs.unifi;
+    unifiPackage = pkgs.unstable.unifi;
     jrePackage = pkgs.jdk11;
     openFirewall = true;
   };
