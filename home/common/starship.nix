31739eb1587aa56cc682351884a6668cb3529948
xyven1 <maritimus987@gmail.com>
2023-03-09 22:01:25 -0500

    fix startship user style

--- home/common/starship.nix
+++ home/common/starship.nix
@@ -3,17 +3,6 @@
   programs.starship = {
     enable = true;
     settings = {
-      # format =
-      #   let
-      #     git = "$git_branch$git_commit$git_state$git_status";
-      #     cloud = "$aws$gcloud$openstack";
-      #   in
-      #   ''
-      #     $username$hostname($shlvl)($cmd_duration) $fill ($nix_shell)$custom
-      #     $directory(${git})(- ${cloud}) $fill $time
-      #     $jobs$character
-      #   '';
-
       right_format = "$time";
 
       fill = {
@@ -24,7 +13,8 @@
       # Core
       username = {
         format = "[$user]($style)";
-        style = "bold green";
+        style_user = "bold green";
+        style_root = "bold red";
       };
       hostname = {
         format = "[@$hostname]($style) ";
