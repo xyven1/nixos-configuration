2c3d70b1fbd7bb44016822da99d80ee3d080c492
xyven1 <git@xyven.dev>
2023-11-27 12:36:47 -0500

    update unifi

--- hosts/ockham/services/unifi.nix
+++ hosts/ockham/services/unifi.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
+  disabledModules = [ "services/networking/unifi.nix" ];
+  imports =
+    [
+      "${inputs.nixpkgs-unstable}/nixos/modules/services/networking/unifi.nix"
+    ];
   services.unifi = {
     enable = true;
-    unifiPackage = pkgs.unstable.unifi;
+    unifiPackage = pkgs.unstable.unifi7;
     openFirewall = true;
   };
 }
