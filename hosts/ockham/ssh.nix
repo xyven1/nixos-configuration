c018569f60875738a696b28388547f02569a3ca8
xyven1 <git@xyven.dev>
2024-02-14 19:20:59 -0500

    ockham: fix ssh.nix

--- hosts/ockham/ssh.nix
+++ hosts/ockham/ssh.nix
@@ -1,4 +1,4 @@
-{}: {
+{ ... }: {
   users.users = {
     xyven.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OUNpoctlB+kygCCqcP/YRPDzGcykblU5TKUnfKhY+ blake@gretchen"
