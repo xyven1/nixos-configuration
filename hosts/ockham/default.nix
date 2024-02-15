96edd173d2257f7a8ecb3f6ecff4c5ddb57cd795
Xyven1 <maritimus987@gmail.com>
2024-02-14 19:16:45 -0500

    ockham: clean up ssh authorizedKeys

--- hosts/ockham/default.nix
+++ hosts/ockham/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, lib, ... }:
+{ inputs, lib, ... }:
 let disks = [ "/dev/nvme0n1" ];
 in
 {
@@ -6,6 +6,7 @@ in
     inputs.vscode-server.nixosModules.default
     ./hardware-configuration.nix
     ./nvidia.nix
+    ./ssh.nix
     ./services
     (import ./disko.nix {
       disks = disks;
@@ -15,18 +16,6 @@ in
     ../common/users/gob
   ];
 
-  users.users.xyven = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OUNpoctlB+kygCCqcP/YRPDzGcykblU5TKUnfKhY+ blake@gretchen"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDqKxEMH57VYdc6hCe25uBkok0KeArgwARqOs1Dw1UBu xyven@festus"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEq5E8HRvArWWc5F9+qI6AuU9Kh1CoJ8/lZ+jErnQAOC xyven@BLAKE-XPS17"
-    ];
-  };
-  users.users.gob = {
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJ9kEMLVISMvJbcuy6WX7ssoWt9UZ8vq/KCOHaB8S78C0Gq6qG94sJwxyQpuZvfFjEEXjC/qay+iJfNWBFh5klLaG9pqS1UIBNTNLvZVZoJl9wwKc29Z0ADPKM5CiNMcwkzCuJ+9lT/z/SbcSh1ptYi9D3apHAVSMeHCQdwOCJ8RVbaSrYGVxqPJvXVvSp6SZmlO03xDZOh8DdY1mUGANRMi8Ds3bwrwxUIwcxFOeSTDavhC8DMD23P94vNkYsf7AbHhgg+57XAgheEqoZRj8FYcmFT5ZqTLAQaMcZNL+H8zwFd1JVPbf6V+5FaPWLZXsB/7kd8OGsMPyoE4WoDujifP7448NOFjBmw45zbmO+N9V4PpvPqswx+FvyOnDIa4hgJuVZoDFt+8WZLrl+L/vhqCE7zK2JXH1zhL17DG0ELkxhenTBTLutBmZyDrbOh7KSx/GwtMoiUq7bcoQdEjxh1re8n0hmFsguqIXuZp1zt3d/OYZbxxGOVxoWY0rIFaM= gob@popper-3-wsl"
-    ];
-  };
   fileSystems."/video-storage" = {
     device = "/dev/mapper/media-volume";
     fsType = "ext4";
