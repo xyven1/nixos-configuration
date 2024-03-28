29253b5dbb3a8609fa771d239999b2c2a5ab39e6
Xyven1 <maritimus987@gmail.com>
2024-03-28 13:24:15 -0400

    move idea-ultimate overlay to new package

--- overlay/default.nix
+++ overlay/default.nix
@@ -8,20 +8,7 @@
       config = config.nixpkgs.config;
     };
   };
-  jetbrains = final: prev: {
-    # Override jetbrains idea-ultimate until the newer version is available
-    jetbrains =
-      prev.jetbrains
-      // {
-        idea-ultimate = prev.jetbrains.idea-ultimate.overrideAttrs (_: {
-          version = "2022.3";
-          src = prev.fetchurl {
-            url = "https://download-cdn.jetbrains.com/idea/ideaIU-2022.3.3.tar.gz";
-            sha256 = "sha256-wwK9hLSKVu8bDwM+jpOg2lWQ+ASC6uFy22Ew2gNTFKY=";
-          };
-        });
-      };
-  };
+
   additions = final: _prev: import ../pkgs {pkgs = final;};
 
   wpi-wireless-install = inputs.wpi-wireless-install.overlays.default;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,11 @@ with pkgs; {
   tlpui = python3Packages.callPackage ./tlpui.nix {};
   sioyek = qt6.callPackage ./sioyek.nix {};
   scenebuilder19 = callPackage ./scenebuilder.nix {};
+  idea-ultimate-latest = pkgs.jetbrains.idea-ultimate.overrideAttrs (_: {
+    version = "2022.3";
+    src = pkgs.fetchurl {
+      url = "https://download-cdn.jetbrains.com/idea/ideaIU-2022.3.3.tar.gz";
+      sha256 = "sha256-wwK9hLSKVu8bDwM+jpOg2lWQ+ASC6uFy22Ew2gNTFKY=";
+    };
+  });
 }
