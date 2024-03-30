a6cd2ff93add1563d19322df682dd3f3b455a51f
xyven1 <git@xyven.dev>
2024-03-30 16:50:11 -0400

    add sops to devShell

--- shell.nix
+++ shell.nix
@@ -16,5 +16,5 @@
 pkgs.mkShell {
   # Enable experimental features without having to specify the argument
   NIX_CONFIG = "experimental-features = nix-command flakes";
-  nativeBuildInputs = with pkgs; [nix home-manager git];
+  nativeBuildInputs = with pkgs; [nix home-manager git sops];
 }
