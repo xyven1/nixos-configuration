72c3012e1e4efb48d44d2d8fd3d9f14201f3cd19
xyven1 <git@xyven.dev>
2024-05-10 03:57:40 -0400

    ockham: mongodb overlay to fix build

--- hosts/ockham/services/unifi.nix
+++ hosts/ockham/services/unifi.nix
@@ -3,6 +3,20 @@
     "unifi-controller"
     "mongodb"
   ];
+  nixpkgs.overlays = [
+    (prev: final: {
+      mongodb-5_0 = final.mongodb-5_0.overrideAttrs (old: {
+        src = final.fetchFromGitHub {
+          owner = "mongodb";
+          repo = "mongo";
+          rev = "r5.0.24";
+          hash = "sha256-SZ62OJD6L3aP6LsTswpuXaayqYbOaSQTgEmV89Si7Xc=";
+        };
+
+        sconsFlags = old.sconsFlags ++ ["MONGO_VERSION=5.0.24"];
+      });
+    })
+  ];
   services.unifi = {
     enable = true;
     unifiPackage = pkgs.unstable.unifi8;
