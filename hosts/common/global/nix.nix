00a9f4f4f8d9a89ae7e9f036985ce7e53c32d99b
xyven1 <git@xyven.dev>
2024-05-16 15:32:50 -0400

    add personal store

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -4,8 +4,14 @@
       auto-optimise-store = lib.mkDefault true;
       experimental-features = ["nix-command" "flakes" "repl-flake"];
       warn-dirty = false;
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://xyven1.cachix.org"
+      ];
+      trusted-public-keys = [
+        "xyven1.cachix.org-1:Eb3g2wdg3iE6nWKzC6OK3IXg54OTokehG539mo1wrmQ="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
     gc = {
       automatic = true;
