ca6385ee3451185834f89e53ee2ddf078ce26b32
xyven1 <git@xyven.dev>
2024-05-16 17:14:11 -0400

    remove hyperland substituters

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -5,12 +5,10 @@
       experimental-features = ["nix-command" "flakes" "repl-flake"];
       warn-dirty = false;
       substituters = [
-        "https://hyprland.cachix.org"
         "https://xyven1.cachix.org"
       ];
       trusted-public-keys = [
         "xyven1.cachix.org-1:Eb3g2wdg3iE6nWKzC6OK3IXg54OTokehG539mo1wrmQ="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
     gc = {
