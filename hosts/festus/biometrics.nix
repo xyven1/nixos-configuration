b33ba8bc7d7dbcfeea643168eae051a31738902e
xyven1 <git@xyven.dev>
2024-09-12 17:39:12 -0400

    festus: pam module -> submodule config override

--- hosts/festus/biometrics.nix
+++ hosts/festus/biometrics.nix
@@ -1,19 +1,26 @@
-{pkgs, ...}: let
-  tod-pkg = "libfprint-2-tod1-goodix";
-in {
-  nixpkgs.allowUnfreePackages = [
-    tod-pkg
-  ];
-  disabledModules = ["security/pam.nix"];
-  imports = [./pam.nix];
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  options.security.pam.services = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule {
+      config.fprintAuth = false;
+    });
+  };
+  config = let
+    tod-pkg = "libfprint-2-tod1-goodix";
+  in {
+    nixpkgs.allowUnfreePackages = [
+      tod-pkg
+    ];
 
-  security.pam.serviceDefaults.fprintAuth = false;
-
-  services.fprintd = {
-    enable = true;
-    tod = {
+    services.fprintd = {
       enable = true;
-      driver = pkgs.${tod-pkg};
+      tod = {
+        enable = true;
+        driver = pkgs.${tod-pkg};
+      };
     };
   };
 }
