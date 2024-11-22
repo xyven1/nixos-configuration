a1daafde2e1189f0f95446551c5cdf7fbaf45df2
xyven1 <git@xyven.dev>
2024-11-21 19:46:15 -0500

    hosts(common): add cuda-maintainers to substitures

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -11,10 +11,12 @@
       warn-dirty = false;
       substituters = [
         "https://xyven1.cachix.org"
+        "https://cuda-maintainers.cachix.org"
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "xyven1.cachix.org-1:Eb3g2wdg3iE6nWKzC6OK3IXg54OTokehG539mo1wrmQ="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
