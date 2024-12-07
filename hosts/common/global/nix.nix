5d8c4430eaf076da0b19c1461a7e772cd24dc476
xyven1 <git@xyven.dev>
2024-12-07 14:41:08 -0500

    hosts(common/global): cuda-maintainers -> nix-community

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -11,12 +11,12 @@
       warn-dirty = false;
       substituters = [
         "https://xyven1.cachix.org"
-        "https://cuda-maintainers.cachix.org"
+        "https://nix-community.cachix.org"
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "xyven1.cachix.org-1:Eb3g2wdg3iE6nWKzC6OK3IXg54OTokehG539mo1wrmQ="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
