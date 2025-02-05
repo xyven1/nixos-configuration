49888312ca76989ccd0d962239cf744832008a70
xyven1 <git@xyven.dev>
2025-02-05 18:13:56 -0500

    hosts(common/global): disable garnix cache

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -12,12 +12,12 @@
       substituters = [
         "https://xyven1.cachix.org"
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
+        # "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "xyven1.cachix.org-1:Eb3g2wdg3iE6nWKzC6OK3IXg54OTokehG539mo1wrmQ="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
     gc = {
