e4eb7a9c658ccd3060b23fb69e550c6927ed0261
xyven1 <git@xyven.dev>
2025-02-10 16:00:08 -0500

    hosts(festus): remove unneeded config

--- hosts/festus/default.nix
+++ hosts/festus/default.nix
@@ -75,7 +75,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
