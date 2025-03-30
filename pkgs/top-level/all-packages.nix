39a38023ad430c086c401161c82d4d86766f0ba9
xyven1 <git@xyven.dev>
2025-03-29 20:09:55 -0400

    pkgs: remove neovide

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2,8 +2,6 @@ self: super:
 with self; {
   dronecan-gui-tool = callPackage ../applications/dronecan-gui-tool {};
 
-  neovide-nightly = callPackage ../applications/neovide {};
-
   scenebuilder19 = callPackage ../applications/scenebuilder {};
 
   tlpui = callPackage ../applications/tlpui {};
