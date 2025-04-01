12f681ca242492fec23a832b9ba26538366dbff8
xyven1 <git@xyven.dev>
2025-03-31 20:04:09 -0400

    pkgs(scenebuilder): remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2,7 +2,5 @@ self: super:
 with self; {
   dronecan-gui-tool = callPackage ../applications/dronecan-gui-tool {};
 
-  scenebuilder19 = callPackage ../applications/scenebuilder {};
-
   tlpui = callPackage ../applications/tlpui {};
 }
