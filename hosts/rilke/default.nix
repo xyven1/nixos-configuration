61252730efbf03c9c28a32b00c5aceb5fec6edd0
xyven1 <git@xyven.dev>
2025-06-23 13:40:06 -0700

    hosts(rilke): purge pulse

--- hosts/rilke/default.nix
+++ hosts/rilke/default.nix
@@ -62,13 +62,11 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
   };
   services.power-profiles-daemon.enable = false;
   services.thermald.enable = true;
