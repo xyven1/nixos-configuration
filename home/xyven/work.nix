56d97e5e6992934c1671b24d92ffe852e8f9cf3d
xyven1 <git@xyven.dev>
2025-07-17 10:10:25 -0600

    home(xyven/work): update for new laptop

--- home/xyven/work.nix
+++ home/xyven/work.nix
@@ -1,55 +1,90 @@
 {
   pkgs,
   inputs,
+  config,
   lib,
   ...
 }: let
-  nixGLWrap = pkg:
-    pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-       wrapped_bin=$out/bin/$(basename $bin)
-       echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \"\$@\"" > $wrapped_bin
-       chmod +x $wrapped_bin
-      done
-    '';
 in {
   imports = [
     ../xyven/generic.nix
-    ../common/gnome.nix
-    ../common/helix.nix
     ../common/ghostty.nix
   ];
 
   nixpkgs = {
     config.allowUnfree = true;
-    overlays = [
-      inputs.nixgl.overlay
-    ];
   };
 
   rbh.profile = "xyven@work";
 
+  nixGL.packages = inputs.nixgl.packages;
+
   home = {
+    username = lib.mkForce "blake";
     packages = with pkgs.unstable; [
-      google-chrome
-      docker
+      slack
     ];
-    # sessionVariables.TERMINFO_DIRS = "${pkgs.ghostty.passthru.terminfo}/share/terminfo";
   };
+
+  neovim.local-config = true;
 
   programs = {
-    # ghostty.package = lib.mkForce (nixGLWrap pkgs.unstable.ghostty);
-    # fish.package = pkgs.fish.override {
-    #   # fix for using fish as login shell
-    #   fishEnvPreInit = source: source "/etc/profile.d/nix.sh";
-    # };
+    ghostty.package = lib.mkForce (config.lib.nixGL.wrap pkgs.unstable.ghostty);
+    fish.package = pkgs.fish.override {
+      fishEnvPreInit = source: "test -f /etc/profile.d/nix.sh && ${source "/etc/profile.d/nix.sh"}";
+    };
+    chromium = {
+      enable = true;
+      package = config.lib.nixGL.wrap pkgs.unstable.google-chrome;
+    };
+    vscode = {
+      enable = true;
+      package = config.lib.nixGL.wrap pkgs.unstable.vscode;
+    };
   };
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Yaru-prussiangreen-dark";
+      package = pkgs.adw-gtk3;
+    };
+  };
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style.name = "adwaita-dark";
+  };
   dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      clock-format = "12h";
+    };
+    "org/gnome/nautilus/list-view" = {
+      default-zoom-level = "medium";
+      use-tree-view = true;
+    };
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "list-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+      search-view = "list-view";
+    };
+    "org/gnome/desktop/default-applications/terminal" = lib.mkIf config.programs.ghostty.enable {
+      exec = lib.getExe config.programs.ghostty.package;
+    };
+    "org/gnome/desktop/peripherals/keyboard" = {
+      delay = lib.hm.gvariant.mkUint32 225;
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+    };
+    "org/gnome/desktop/input-sources" = {
+      xkb-options = ["caps:swapescape"];
+    };
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "appmenu:minimize,maximize,close";
     };
