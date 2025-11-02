45acf255edcb66d8273e824772a016d41930958f
xyven1 <git@xyven.dev>
2025-11-02 01:59:11 -0600

    hosts(rilke): vid accel for nvidia is enabed by default

--- hosts/rilke/nvidia.nix
+++ hosts/rilke/nvidia.nix
@@ -1,11 +1,7 @@
-{pkgs, ...}: {
+{...}: {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
   };
 
   services.xserver.videoDrivers = ["nvidia"];
