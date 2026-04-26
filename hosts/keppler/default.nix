786e9bb25f801e2e8b02fe0d62a1705a1d49b7c6
xyven1 <git@xyven.dev>
2026-04-26 15:05:05 +0000

    hosts(keppler): more robust fan speed script

--- hosts/keppler/default.nix
+++ hosts/keppler/default.nix
@@ -24,11 +24,27 @@
       Type = "oneshot";
       RemainAfterExit = true;
     };
-    path = with pkgs; [coreutils ipmitool];
+    path = with pkgs; [coreutils gawk ipmitool];
     script = ''
-      ipmitool raw 0x30 0x45 0x01 0x01
-      sleep 1
-      ipmitool raw 0x30 0x70 0x66 0x01 0x00 0x12
+      NUM_GOOD=4
+      TARGET_FAN_SPD=5000
+
+      GOOD_COUNT=0
+      while [ $GOOD_COUNT -lt $NUM_GOOD ]; do
+        FAN_SPD=$(ipmitool sensor reading "FAN1" | awk '{ print $3 }')
+        if [ "$FAN_SPD" -gt "$TARGET_FAN_SPD" ]; then
+          echo "Fan speed is too high ($FAN_SPD > $TARGET_FAN_SPD), setting..."
+          ipmitool raw 0x30 0x45 0x01 0x01
+          sleep 1
+          ipmitool raw 0x30 0x70 0x66 0x01 0x00 0x12
+          GOOD_COUNT=0
+        else
+          echo "Fan speed is good ($FAN_SPD <= $TARGET_FAN_SPD)"
+          GOOD_COUNT=$((GOOD_COUNT + 1))
+        fi
+        sleep 5
+      done
+      echo "Fan speed is stable, exiting."
     '';
   };
 
