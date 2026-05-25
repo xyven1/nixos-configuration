cd12444dc59156fd3c2c3e5cfc730f00581d954c
xyven1 <git@xyven.dev>
2026-05-25 04:05:47 -0600

    hosts(festus): remove defunct nvidia suspend fixes

--- hosts/festus/nvidia.nix
+++ hosts/festus/nvidia.nix
@@ -15,15 +15,6 @@
     "nvidia-x11"
   ];
 
-  systemd.services = let
-    append = ["systemd-suspend-then-hibernate.service"];
-  in {
-    nvidia-suspend.before = append;
-    nvidia-suspend.requiredBy = append;
-    nvidia-resume.after = append;
-    nvidia-resume.requiredBy = append;
-  };
-
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     powerManagement.enable = true;
