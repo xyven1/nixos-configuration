254e15c8d7955b19fdfb86aa74f20602877d7cb9
xyven1 <git@xyven.dev>
2026-06-06 20:44:22 +0000

    hosts(common/kmscon): hwaccel if graphics hw enabled

--- hosts/common/optional/kmscon.nix
+++ hosts/common/optional/kmscon.nix
@@ -1,10 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
   fonts.packages = [pkgs.nerd-fonts.jetbrains-mono];
   services.kmscon = {
     enable = true;
     config = {
       font-name = "JetBrainsMono Nerd Font";
-      hwaccel = true;
+      hwaccel = lib.mkIf config.hardware.graphics.enable true;
     };
   };
 }
