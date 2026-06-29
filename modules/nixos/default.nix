1740ad0b1ec06508aeea19f6f4ec7fc98391e089
xyven1 <git@xyven.dev>
2026-06-28 20:37:29 -0600

    modules(nixos): remove unpackerr in favor of upstream

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,6 +2,5 @@
   nixpkgs = import ../shared/nixpkgs.nix;
   clear-linux = import ./clear-linux.nix;
   vopono = import ./vopono.nix;
-  unpackerr = import ./unpackerr.nix;
   nginx = import ./nginx.nix;
 }
